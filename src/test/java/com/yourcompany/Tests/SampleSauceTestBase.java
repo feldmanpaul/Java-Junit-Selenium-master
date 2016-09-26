ad5b33ef3ac64ef4ba1c50d7b82d83faefcebb5a
Paul <paul@saucelabs.com>
2016-09-26 11:23:54 -0400

    monday morning

--- src/test/java/com/yourcompany/Tests/SampleSauceTestBase.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTestBase.java
@@ -171,8 +171,6 @@ public class SampleSauceTestBase implements SauceOnDemandSessionIdProvider {
 
         browsers.add(new String[]{"Mac 10.10", "9.2", "iPhone", "iPhone 6", "portrait"});
 
-
-
         return browsers;
     }
 
